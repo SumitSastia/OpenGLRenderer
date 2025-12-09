110fdf6444d9914af923b1343df791a024611749
Sumit Sastia <sumitsastiya2405@gmail.com>
2025-12-09 20:19:14 +0530

    Initialized GLFW

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/firstproject")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/GLFWproject")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -30,11 +30,6 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/sumit/Documents/GitHub/OpenGLRenderer/build/libs/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include <h1.hpp>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 int main(){
-    cout << getnum() << endl;
+    
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(800,600,"FirstWindow 1.0", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window)){
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
