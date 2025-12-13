98aa85b7c99b237df7e5371ee7470f673e90eb19
Sumit Sastia <sumitsastiya2405@gmail.com>
2025-12-13 17:44:59 +0530

    Multiple 3D-Cubes

--- src/main.cpp
+++ src/main.cpp
@@ -185,6 +185,19 @@ int main(){
     view = glm::translate(view, glm::vec3(0.0f,0.0f,-3.0f));
     projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
     bool isRunning = true;
     
     // OPENGL LOOP --------------------------------------------------------------------//
@@ -207,13 +220,23 @@ int main(){
 
         // glUniform1i(glGetUniformLocation(shaderProgram,"ourTexture1"), 0);
 
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"model"), 1, GL_FALSE, glm::value_ptr(model));
+        // glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"model"), 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"projection"), 1, GL_FALSE, glm::value_ptr(projection));
         
         glBindVertexArray(VAO);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        for(unsigned int i=0; i<10; i++){
+
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+
+            float angle = 30.0*i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f,0.3f,0.5f));
+
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"model"), 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // Events //
         glfwPollEvents();
